e2a479e9e98f9ee31ef2f2aa476912579e092884
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-07 23:30:09 +0900

    クラス自体の情報を表示する

--- main.ts
+++ main.ts
@@ -10,12 +10,17 @@ class User{
   sayHi(): void{       //これがメソッド
     console.log("hi! i am " + this.name);  //クラス内の変数を呼ぶ時は this を使う
   }
-  static count: number = 0;                 //静的メンバのインスタンスを数える
+  static count: number = 0;                    //静的メンバのインスタンスを数える
+  static showDescription(): void{              //このクラス自体の情報を表示するメソッド
+    console.log("this class is about users");  //このクラス自体の情報を表示する
+  }
 }
 
 var tom = new User("Tom");
 var bob = new User("Bob");
 console.log(User.count);
+User.showDescription();                        //このクラス自体の情報を呼び出している
 
 //実行結果は下記
 //2
+//this class is about users
