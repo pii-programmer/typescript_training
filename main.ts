b2ad9899bf8f8f4bbf3dbd75ac26988641b4ea90
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-07 22:31:24 +0900

    クラスを継承する方法

--- main.ts
+++ main.ts
@@ -1,32 +1,26 @@
-//クラスベースのオブジェクト指向でプログラミングしてみる
-//アクセス修飾子privateは、クラス外には呼び出せない
+//クラスを継承する
 
-//クラスには、変数とメソッドをまとめておける
 class User{
-  constructor(private _name:string){
+  constructor(private _name:string){  //Userクラスの変数name
   }
   public sayHi(): void{  //これがメソッド
     console.log("hi! i am " + this._name);  //クラス内の変数を呼ぶ時は this を使う
   }
 
-  get name(){
-    return this._name;
+//クラスの継承の仕方
+class AdminUser extends User{  //これでUserクラスの変数やメソッドを保持しつつ、新しいクラスを作る
+//新しいAdminUserクラスが、Userクラスを継承した。この{}内に変数やメソッドをまとめられる
+  private _age:number;                     //AdminUserクラスの変数ageを宣言
+  constructor(_name:string, _age:number){  //インスタンス化する時に呼び出す変数はnameとage
+    super (_name);                         //Userという親クラスの_nameにはsuperを
+    this._age = _age;                      //クラス内の_ageを呼ぶのでthis
   }
-//privateだけどクラス外で変数nameを更新する時はsetterを使う
-  set name(newValue: string){
-    this._name = newValue;
-  }
-};
-
-var ami = new User("Ami");  //インスタンス化する
-console.log(ami.name);
-ami.name = "AMI";       //"AMI"をnewValueに引き渡す
-console.log(ami.name);  //setterされた変数nameを呼び出す
-ami.sayHi();            //メソッドsayHiはpublicなのでクラス外でも呼び出せる
+}
 
 
-//このままコンパイルするとエラーになるので、%tsc main.ts -t ES5 コマンドで実行する
+
+
 //実行結果は下記
-//Ami
-//AMI
-//hi! i am Ami
+//
+//
+//
