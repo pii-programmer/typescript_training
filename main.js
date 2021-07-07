8284674fe7fa765d1ec2bd63db4365a1255075e7
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-07 19:33:14 +0900

    関数のオーバーロードとシグネチャ

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-//TypeScriptのアロー関数の例
-var add = function (a, b) { return a + b; };
+//関数のオーバーロード（引数や戻り値が異なる同じ名前の関数を宣言できる）
+//そのためにシグネチャを作る
+//処理
+function add(a, b) {
+    if (typeof a === "string" && typeof b === "string") {
+        return a + b;
+    }
+    return a + b;
+}
 //呼び出す
-console.log(add(5, 3));
+console.log(add(5, 3)); //8
+console.log(add("hello", "world")); //helloworld
--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-//TypeScriptのアロー関数の例
+//関数のオーバーロード（引数や戻り値が異なる同じ名前の関数を宣言できる）
+//そのためにシグネチャを作る
 
-var add = (a: number, b: number): number => a + b;
+function add(a:number, b:number):number;  //これがシグネチャ
+function add(a:string, b:string):string;
+
+//処理
+function add(a:any, b:any):any{
+  if(typeof a === "string" && typeof b === "string"){
+    return a + b;
+  }
+  return a + b;
+}
 
 //呼び出す
-console.log(add(5,3));
+console.log(add(5,3));  //8
+console.log(add("hello","world"));  //helloworld
