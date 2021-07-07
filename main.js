fa34d88ae599a19956c972fb5170c8748b2d4377
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-07 18:40:08 +0900

    新たに型を列挙型で定義する時

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var User = (function () {
-    function User() {
-    }
-    return User;
-})();
-console.log("hello world");
+//class User {
+//  
+//}
+//console.log("hello world");
+//Signalという型を列挙型で定義する
+var Signal;
+(function (Signal) {
+    Signal[Signal["Red"] = 0] = "Red";
+    Signal[Signal["Blue"] = 1] = "Blue";
+    Signal[Signal["Yellow"] = 2] = "Yellow";
+})(Signal || (Signal = {}));
+var result;
+//処理は例えばこのようにかく
+if (result === Signal.Yellow) {
+    //処理
+}
+console.log(Signal[2]); //Yellow
--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-class User {
-  
+//class User {
+//  
+//}
+//console.log("hello world");
+
+
+//Signalという型を列挙型で定義する
+enum Signal{
+  Red = 0,
+  Blue = 1,
+  Yellow = 2
 }
 
-console.log("hello world");
+var result: Signal;
+
+//処理は例えばこのようにかく
+if (result === Signal.Yellow){
+  //処理
+}
+
+console.log(Signal[2]); //Yellow
