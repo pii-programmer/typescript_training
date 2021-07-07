04811f31a97eef58054a0ec26b6d0cf01076b950
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-08 08:55:37 +0900

    インターフェースの継承

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-//Interfaceとは渡ってくるオブジェクトの型に名前を付けること
-//構造的部分型
+//Interfaceの継承
+//インターフェースは複数のインターフェースから継承できる
 function getTotal(result) {
-    return result.a + result.b;
+    return result.a + result.b + result.final;
 }
 var result = {
     a: 40,
     b: 50,
-    c: "Hello" //ここに何らかのプロパティが入っていたとしても、resultがaとbを持っていれば、Result型とみなされる。これが構造的部分型
+    final: 90
 };
 console.log(getTotal(result));
 //実行結果は
-//90
+//180
--- main.ts
+++ main.ts
@@ -1,23 +1,30 @@
-//Interfaceとは渡ってくるオブジェクトの型に名前を付けること
-//構造的部分型
+//Interfaceの継承
+//インターフェースは複数のインターフェースから継承できる
 
-interface Result{
+interface SpringResult{
   a:number;
+}
+
+interface FallResult{
   b:number;
 }
 
-function getTotal(result: Result){
-  return result.a + result.b;
+interface FinalResult extends SpringResult, FallResult{
+  final:number;
+}
+
+function getTotal(result: FinalResult){
+  return result.a + result.b + result.final;
 }
 
 var result = {
   a: 40,
   b: 50,
-  c: "Hello"  //ここに何らかのプロパティが入っていたとしても、resultがaとbを持っていれば、Result型とみなされる。これが構造的部分型
+  final: 90
 }
 
 console.log(getTotal(result));
 
 
 //実行結果は
-//90
+//180
