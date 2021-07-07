ceb84398d64e35a5d21cf9b17d3c5af54c1ba4d6
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-07 19:00:05 +0900

    関数の定義の仕方〜基本〜

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-//class User {
-//  
-//}
-//console.log("hello world");
-//Signalという型を列挙型で定義する
-//enum Signal{
-//  Red = 0,
-//  Blue = 1,
-//  Yellow = 2
-//}
-//数を省略してかくと
-var Signal;
-(function (Signal) {
-    Signal[Signal["Red"] = 0] = "Red";
-    Signal[Signal["Blue"] = 3] = "Blue";
-    Signal[Signal["Yellow"] = 4] = "Yellow";
-})(Signal || (Signal = {}));
-var result;
-//処理は例えばこのようにかく
-if (result === Signal.Yellow) {
-    //処理
+//関数の定義例
+function add(a, b) {
+    return a + b;
 }
-console.log(Signal[4]); //Blue
+//関数の呼び出し方例
+console.log(add(5, 3));
--- main.ts
+++ main.ts
@@ -1,29 +1,8 @@
-//class User {
-//  
-//}
-//console.log("hello world");
+//関数の定義例
 
-
-//Signalという型を列挙型で定義する
-//enum Signal{
-//  Red = 0,
-//  Blue = 1,
-//  Yellow = 2
-//}
-
-//数を省略してかくと
-enum Signal{
-  Red,
-  Blue = 3,
-  Yellow
+function add(a, b){
+  return a + b;
 }
 
-
-var result: Signal;
-
-//処理は例えばこのようにかく
-if (result === Signal.Yellow){
-  //処理
-}
-
-console.log(Signal[4]); //Yellow
+//関数の呼び出し方例
+console.log(add(5,3));
