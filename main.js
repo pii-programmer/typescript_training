ce8fb1e9275e7deebd6afa4e499423dcb3831f72
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-08 10:13:38 +0900

    Genericsはクラスやインターフェースにも使える

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 //Generics：抽象化されたデータ型
+//関数だけでなく、クラスやインターフェースにもGenericsを行える
 //具体例：
-//関数を定義する。関数の処理内に、ある変数に3つの値が入った配列を返す、という処理を書く。
-function getStringArray(value) {
-    return [value, value, value];
-}
-//ではstringだけでなくnumberにも同じ処理をしたい場合、下記のように書けるが、
-function getNumberArray(value) {
-    return [value, value, value];
-}
-//これを複数やっていたらキリがない。
-//そこで、Genericsを行う。
-function getArray(value) {
-    return [value, value, value];
-}
-console.log(getArray(3)); //<T> = <number> Typeはnumber型です。(3)は引数です。
-console.log(getArray("Hello")); //<T> = <string> Typeはstring型です。("Hello")は引数です。
+var MyData = /** @class */ (function () {
+    function MyData(value) {
+        this.value = value;
+    }
+    MyData.prototype.getArray = function () {
+        return [this.value, this.value, this.value];
+    };
+    return MyData;
+}());
+var v1 = new MyData("Hello"); //インスタンスを生成
+console.log(v1.getArray());
+var v2 = new MyData(3); //インスタンスを生成
+console.log(v2.getArray());
 //実行結果は
-//[ 3, 3, 3 ]
 //[ 'Hello', 'Hello', 'Hello' ]
+//[ 3, 3, 3 ]
--- main.ts
+++ main.ts
@@ -1,27 +1,19 @@
 //Generics：抽象化されたデータ型
+//関数だけでなく、クラスやインターフェースにもGenericsを行える
 
 //具体例：
-//関数を定義する。関数の処理内に、ある変数に3つの値が入った配列を返す、という処理を書く。
-function getStringArray(value:string): string[]{
-  return [value, value, value];
+class MyData<T>{
+  constructor (public value:T){}
+  getArray(): T[]{
+    return [this.value, this.value, this.value];
+  }
 }
 
-//ではstringだけでなくnumberにも同じ処理をしたい場合、下記のように書けるが、
-function getNumberArray(value:number): number[]{
-  return [value, value, value];
-}
-
-//これを複数やっていたらキリがない。
-//そこで、Genericsを行う。
-
-function getArray<T>(value:T): T[]{             //TとはTypeのこと
-  return [value, value, value];
-}
-
-console.log(getArray<number>(3));              //<T> = <number> Typeはnumber型です。(3)は引数です。
-console.log(getArray<string>("Hello"));        //<T> = <string> Typeはstring型です。("Hello")は引数です。
-
+var v1 = new MyData<string>("Hello");  //インスタンスを生成
+console.log(v1.getArray());
+var v2 = new MyData<number>(3);  //インスタンスを生成
+console.log(v2.getArray());
 
 //実行結果は
-//[ 3, 3, 3 ]
 //[ 'Hello', 'Hello', 'Hello' ]
+//[ 3, 3, 3 ]
