0ee11f47eb3e0b13f56c030424b84ff5edd83070
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-08 10:04:20 +0900

    Genericsを理解する

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-//インターフェースとクラスを組み合わせる
-//Interface -> Class
-//これがクラスの定義
-var User = /** @class */ (function () {
-    function User(name) {
-        this.score = 0; //変数scoreに初期値を設定してあげる
-        this.name = name;
-    }
-    User.prototype.sayHi = function () {
-        console.log("hi! i am " + this.name);
-    };
-    User.prototype.showScore = function () {
-        console.log("score " + this.score); //メソッドshowScoreの処理内容を設定している
-    };
-    return User;
-}());
+//Generics：抽象化されたデータ型
+//具体例：
+//関数を定義する。関数の処理内に、ある変数に3つの値が入った配列を返す、という処理を書く。
+function getStringArray(value) {
+    return [value, value, value];
+}
+//ではstringだけでなくnumberにも同じ処理をしたい場合、下記のように書けるが、
+function getNumberArray(value) {
+    return [value, value, value];
+}
+//これを複数やっていたらキリがない。
+//そこで、Genericsを行う。
+function getArray(value) {
+    return [value, value, value];
+}
+console.log(getArray(3)); //<T> = <number> Typeはnumber型です。(3)は引数です。
+console.log(getArray("Hello")); //<T> = <string> Typeはstring型です。("Hello")は引数です。
+//実行結果は
+//[ 3, 3, 3 ]
+//[ 'Hello', 'Hello', 'Hello' ]
--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-//インターフェースとクラスを組み合わせる
-//Interface -> Class
+//Generics：抽象化されたデータ型
 
-//これがインターフェースの定義
-interface GameUser{
-  score: number;                  //scoreという変数
-  showScore(): void;              //showScoreというメソッド
+//具体例：
+//関数を定義する。関数の処理内に、ある変数に3つの値が入った配列を返す、という処理を書く。
+function getStringArray(value:string): string[]{
+  return [value, value, value];
 }
 
-//これがクラスの定義
-class User implements GameUser {  //implements == クラスUserは、インターフェースGameUserの変数とメソッドを必ず持つこと。
-  name: string;
-  score :number = 0;                         //変数scoreに初期値を設定してあげる
-  constructor(name: string) {
-      this.name = name;
-  }
-  sayHi(): void {
-      console.log("hi! i am " + this.name);
-  }
-  showScore(): void {
-      console.log("score " + this.score);    //メソッドshowScoreの処理内容を設定している
-  }
+//ではstringだけでなくnumberにも同じ処理をしたい場合、下記のように書けるが、
+function getNumberArray(value:number): number[]{
+  return [value, value, value];
 }
+
+//これを複数やっていたらキリがない。
+//そこで、Genericsを行う。
+
+function getArray<T>(value:T): T[]{             //TとはTypeのこと
+  return [value, value, value];
+}
+
+console.log(getArray<number>(3));              //<T> = <number> Typeはnumber型です。(3)は引数です。
+console.log(getArray<string>("Hello"));        //<T> = <string> Typeはstring型です。("Hello")は引数です。
+
+
+//実行結果は
+//[ 3, 3, 3 ]
+//[ 'Hello', 'Hello', 'Hello' ]
